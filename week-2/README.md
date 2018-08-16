866352898afd61dcd6a9cea11563c1b4af96e4fe
Charles Ellis <hamled@hamled.name>
2018-08-15 18:52:27 -0700

    Week 2 - Test suite

--- week-2/README.md
+++ week-2/README.md
@@ -7,4 +7,9 @@ Topics:
 * Debugging with Pry
 
 Additional questions:
-1.
+1. Access pieces of an array of Hashes
+2. Tips for reading complicated loops
+3. Converting array to hash or vice versa
+4. Until and while loops
+5. Pry and minitest / specs
+6. How/why we would have multiple assertions in one test
--- week-2/enumerables.rb
+++ week-2/enumerables.rb
@@ -1,15 +1,37 @@
+require 'minitest/spec'
+require 'minitest/autorun'
+require 'pry'
+
 PETS_IN_SPACE = [
   { name: "Homer",   home_planet: "Neptune", age:   5, species: :cat },
   { name: "Max",     home_planet: "Earth",   age:   2, species: :dog },
   { name: "Slither", home_planet: "Mars",    age: 105, species: :iguana },
 ]
 
-def average_age
+def average_age(pets)
   total_age = 0
-  PETS_IN_SPACE.each do |pet|
+  pets.each do |pet|
     total_age += pet[:age]
   end
 
-  average_age = total_age / PETS_IN_SPACE.length
+
+  average_age = total_age / pets.length
+
   return average_age
+end
+
+
+describe :average_age do
+  it 'calculates average correctly' do
+    # Arrange
+    pets = [
+      { name: "Test Pet", home_planet: "Earth", age: 30, species: :test}
+    ]
+
+    # Act
+    result = average_age(pets)
+
+    # Assert
+    expect(result).must_equal 30
+  end
 end
