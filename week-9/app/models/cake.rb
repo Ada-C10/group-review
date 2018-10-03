98d7855f73a117ee1331de010f84c1c55775a760
Charles Ellis <hamled@hamled.name>
2018-10-03 16:29:35 -0700

    Week 9 - Validations

--- week-9/app/models/cake.rb
+++ week-9/app/models/cake.rb
@@ -1,2 +1,5 @@
 class Cake < ApplicationRecord
+  validates :message, :pickup, presence: true
+
+  validates :size, inclusion: { in: %w(small medium large x-large) }
 end
--- week-9/app/models/chef.rb
+++ week-9/app/models/chef.rb
@@ -1,2 +1,3 @@
 class Chef < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
 end
